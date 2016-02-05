efd6b0202ca2e7f50096a8c4363f973b3218b048
Christopher Paterson <cjpaterson.3@gmail.com>
2016-02-05 16:12:26 +0000

    Create separate pages for signup and login.

--- iOS/Geo Snap/Geo Snap/SignUpViewController.swift
+++ iOS/Geo Snap/Geo Snap/SignUpViewController.swift
@@ -1,21 +1,21 @@
 //
-//  LoginViewController.swift
+//  SignUpViewController.swift
 //  Geo Snap
 //
-//  Created by Christopher Paterson on 02/02/2016.
+//  Created by Christopher Paterson on 05/02/2016.
 //  Copyright © 2016 Christopher Paterson. All rights reserved.
 //
 
 import UIKit
 import Parse
 
-class LoginViewController: UIViewController {
-    
+class SignUpViewController: UIViewController {
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var retypePassword: UITextField!
     
     var spinner: UIActivityIndicatorView = UIActivityIndicatorView()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -25,11 +25,13 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func login(sender: UIButton) {
+    @IBAction func signUp(sender: UIButton) {
         // Ensure user has entered a username and password.
         if username.text == "" || password.text == "" {
             displayAlert("Error", message: "You must fill in all fields.")
-        
+            
+        } else if password.text != retypePassword.text {
+            displayAlert("Error", message: "Passwords do not match.")
         } else {
             // Let the user know something is happening
             spinner = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
@@ -48,17 +50,36 @@ class LoginViewController: UIViewController {
             // Default error message in case Parse does not return one.
             var errorMessage = "Please try again later."
             
+            // Attempt sign up.
+            user.signUpInBackgroundWithBlock({ (success, error) -> Void in
+                self.spinner.stopAnimating()
+                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                
+                if success {
+                    // Sign up successfull
+                } else {
+                    // error is optional so check exists first
+                    if let signUpError = error?.userInfo["error"] as? String {
+                        errorMessage = signUpError
+                    }
+                    
+                    self.displayAlert("Sign up error", message: errorMessage)
+                }
+            })
         }
+
     }
     
-    @IBAction func signUp(sender: UIButton) {
+    
+    @IBAction func returnToLogin(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     func displayAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
         
         alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
-            self.dismissViewControllerAnimated(true, completion: nil)
+//            self.dismissViewControllerAnimated(true, completion: nil)
         }))
         
         self.presentViewController(alert, animated: true, completion: nil)
