fa2f9d5200f2ccf906e7c5d05114a88a38067d15
Christopher Paterson <cjpaterson.3@gmail.com>
2016-02-15 17:22:34 +0000

    Comments working

---
+++ iOS/GeoSnap/GeoSnap/CommentTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CommentTableViewCell.swift
+//  GeoSnap
+//
+//  Created by Christopher Paterson on 15/02/2016.
+//  Copyright © 2016 Christopher Paterson. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var commentCreator: UILabel!
+    @IBOutlet weak var comment: UILabel!
+    
+}
--- iOS/GeoSnap/GeoSnap/ViewPhotoViewController.swift
+++ iOS/GeoSnap/GeoSnap/ViewPhotoViewController.swift
@@ -11,20 +11,29 @@
 import UIKit
 import Parse
 
-class ViewPhotoViewController: ViewControllerParent {
+class ViewPhotoViewController: ViewControllerParent, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var creatorUsername: UILabel!
     @IBOutlet weak var postComment: UILabel!
     @IBOutlet weak var postPhoto: UIImageView!
-
+    
+    @IBOutlet weak var commentOnPost: UITextField!
+    @IBOutlet weak var commentsTableView: UITableView!
+    
     var postId: String = String()
+    var commentsForPost = [PFObject]()
+    
     
     private var post: PFObject = PFObject(className: "Post")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        commentsTableView.dataSource = self
+        commentsTableView.delegate = self
+        
         retrievePost()
+        retrieveCommentsForPost()
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,7 +42,7 @@ class ViewPhotoViewController: ViewControllerParent {
     }
     
     func retrievePost() {
-        let query = PFQuery(className:"Post")
+        let query = PFQuery(className: "Post")
         query.includeKey("creator")
         query.getObjectInBackgroundWithId(postId) {
             (post: PFObject?, error: NSError?) -> Void in
@@ -73,6 +82,70 @@ class ViewPhotoViewController: ViewControllerParent {
                 }
             }
         }
+    }
+    
+    func retrieveCommentsForPost() {
+        let query = PFQuery(className: "Comment")
+        query.includeKey("creator")
+        query.whereKey("forPost", equalTo: postId)
+        query.orderByDescending("createdAt")
+        
+        query.findObjectsInBackgroundWithBlock { (comments, error) in
+            if error == nil && comments != nil {
+                self.commentsForPost = comments!
+                self.commentsTableView.reloadData()
+            } else {
+                var errorMessage = "Could not retrieve comments at this time. Please try again." // Defaul error message
+                
+                if let errorString = error?.userInfo["error"] as? String {
+                    errorMessage = errorString
+                }
+                
+                self.displayAlert("Error retrieving comments", message: errorMessage)
+            }
+        }
+    }
+    
+    @IBAction func submitComment(sender: UIButton) {
+        let userComment = PFObject(className: "Comment")
+        userComment["comment"] =  commentOnPost.text
+        userComment["creator"] = PFUser.currentUser()
+        userComment["forPost"] = postId
+        
+        userComment.saveInBackgroundWithBlock { (success, error) -> Void in
+            if success {
+                
+                
+            } else {
+                var errorMessage = "Unable to save comment at this time. Please try again later." // Default error message in case Parse does not return one.
+                
+                // error is optional so check exists first
+                if let savePostError = error?.userInfo["error"] as? String {
+                    errorMessage = savePostError
+                }
+                
+                self.displayAlert("Error saving post", message: errorMessage)
+            }
+        }
+    }
+
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return commentsForPost.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("postComment", forIndexPath: indexPath) as! CommentTableViewCell
+        let comment = commentsForPost[indexPath.row]
+        
+        cell.commentCreator.text = comment["creator"].username
+        cell.comment.text = comment["comment"] as? String
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("clicked")
     }
     
     
